ccadba0e04d6c5476ea6961b733542b00f7f1b71
Alexey Sklyarov <asklyarov@ubic.tech>
2025-06-25 18:17:21 +0300

    test: try to fix static test

--- internal/server/test/gauge_test.go
+++ internal/server/test/gauge_test.go
@@ -12,6 +12,7 @@ func TestGauge(t *testing.T) {
 
 	for _, test := range providerTestGauge() {
 		resp, err := I.DoRequest(test.method, test.path, nil)
+		defer resp.Close()
 		assert.NoError(t, err)
 		assert.NotNil(t, resp)
 		assert.Equal(t, test.status, resp.StatusCode)
