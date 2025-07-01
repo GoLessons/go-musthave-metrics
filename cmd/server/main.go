e3d9487bf954c23f753b6e978b2b47dae9c5f3dc
Alexey Sklyarov <asklyarov@ubic.tech>
2025-07-01 10:59:41 +0300

    feat: server adress argument

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"github.com/GoLessons/go-musthave-metrics/internal/server/router"
+	"github.com/spf13/cobra"
 	"net/http"
 )
 
+var address string
+
+var rootCmd = &cobra.Command{
+	Use: "server",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return run()
+	},
+}
+
+func init() {
+	rootCmd.Flags().StringVarP(&address, "address", "a", "localhost:8080", "Metric server address")
+	rootCmd.FParseErrWhitelist.UnknownFlags = false
+}
+
 func main() {
-	if err := run(); err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		panic(err)
 	}
 }
 
 func run() error {
-	return http.ListenAndServe(`:8080`, router.InitRouter())
+	return http.ListenAndServe(address, router.InitRouter())
 }
