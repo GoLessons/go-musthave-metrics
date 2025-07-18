164a6358d44838074a52d001275f4387c17adde7
Alexey Sklyarov <asklyarov@ubic.tech>
2025-07-18 13:09:15 +0300

    fix: use const not magic string

--- internal/server/handler/metrics_handler.go
+++ internal/server/handler/metrics_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"github.com/GoLessons/go-musthave-metrics/internal/model"
+	"github.com/GoLessons/go-musthave-metrics/internal/server"
 	"github.com/GoLessons/go-musthave-metrics/internal/server/service"
 	"github.com/goccy/go-json"
 	"net/http"
@@ -19,7 +20,7 @@ func NewMetricsController(metricService service.MetricService) *metricsControlle
 
 func (h *metricsController) Get(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	metricData := ctx.Value("metric").(model.Metrics)
+	metricData := ctx.Value(server.Metric).(model.Metrics)
 
 	metric, err := h.metricService.Read(metricData.MType, metricData.ID)
 	if err != nil {
@@ -43,7 +44,7 @@ func (h *metricsController) Get(w http.ResponseWriter, r *http.Request) {
 
 func (h *metricsController) Update(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	metricData := ctx.Value("metric").(model.Metrics)
+	metricData := ctx.Value(server.Metric).(model.Metrics)
 
 	err := h.metricService.Save(metricData)
 	if err != nil {
