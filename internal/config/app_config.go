a7f663388ec9bf9c8d8ca21b701ea78d68625cbe
Alexey Sklyarov <asklyarov@ubic.tech>
2025-07-28 12:57:58 +0300

    fix: ignore undefined flags

--- internal/config/app_config.go
+++ internal/config/app_config.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type Config struct {
@@ -20,7 +21,7 @@ type DumpConfig struct {
 }
 
 func LoadConfig(args *map[string]any) (*Config, error) {
-	flags := flag.NewFlagSet("app-config", flag.ExitOnError)
+	flags := flag.NewFlagSet("app-config", flag.ContinueOnError)
 
 	address := flags.String("address", "localhost:8080", "HTTP server address")
 	restore := flags.Bool("restore", false, "Restore metrics before starting")
@@ -34,9 +35,15 @@ func LoadConfig(args *map[string]any) (*Config, error) {
 	flags.StringVar(fileStoragePath, "f", *fileStoragePath, "File storage path (short)")
 	flags.StringVar(databaseDsn, "d", *databaseDsn, "Database DSN")
 
-	err := flags.Parse(os.Args[1:])
+	filteredArgs := filterArgs(flags, os.Args[1:])
+
+	fmt.Printf("Args: %v", filteredArgs)
+
+	err := flags.Parse(filteredArgs)
 	if err != nil {
-		return nil, err
+		if strings.Contains(err.Error(), "flag provided but not defined") {
+			return nil, err
+		}
 	}
 
 	cfg := &Config{
@@ -88,6 +95,30 @@ func LoadConfig(args *map[string]any) (*Config, error) {
 	}
 
 	return cfg, nil
+}
+
+func filterArgs(flags *flag.FlagSet, args []string) []string {
+	var filteredArgs []string
+	validFlags := make(map[string]bool)
+
+	flags.VisitAll(func(f *flag.Flag) {
+		validFlags["-"+f.Name] = true
+	})
+
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+		if len(arg) > 1 && arg[0] == '-' {
+			if _, valid := validFlags[arg]; valid {
+				filteredArgs = append(filteredArgs, arg)
+				if i+1 < len(args) && args[i+1][0] != '-' {
+					filteredArgs = append(filteredArgs, args[i+1])
+					i++ // Пропустить значение
+				}
+			}
+		}
+	}
+
+	return filteredArgs
 }
 
 func redefineLocal(args *map[string]any, cfg *Config) {
