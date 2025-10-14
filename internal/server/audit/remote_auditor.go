99a966dd9696ffa7c9e59d381ef86f8ded0231b0
Alexey Sklyarov <asklyarov@ubic.tech>
2025-10-14 18:52:46 +0300

    fix: http auditor impossible removed

--- internal/server/audit/remote_auditor.go
+++ internal/server/audit/remote_auditor.go
@@ -17,3 +17,51 @@ func NewRemoteAuditor(url string) *RemoteAuditor {
 func (a *RemoteAuditor) Journal(ctx context.Context, item *JournalItem) bool {
 	return true
 }
+
+/**
+// possible implementation
+
+package audit
+
+import (
+	"context"
+	"github.com/goccy/go-json"
+	"resty.dev/v3"
+	"time"
+)
+
+type RemoteAuditor struct {
+	url    string
+	client *resty.Client
+}
+
+func NewRemoteAuditor(url string) *RemoteAuditor {
+	client := resty.New()
+	client.SetTimeout(5 * time.Second).
+		SetHeader("Content-Type", "application/json")
+
+	return &RemoteAuditor{url: url, client: client}
+}
+
+func (a *RemoteAuditor) Journal(ctx context.Context, item *JournalItem) bool {
+	if a.url == "" || item == nil {
+		return false
+	}
+
+	data, err := json.Marshal(item)
+	if err != nil {
+		return false
+	}
+
+	resp, err := a.client.R().
+		WithContext(ctx).
+		SetHeader("Content-Type", "application/json").
+		SetBody(data).
+		Post(a.url)
+	if err != nil {
+		return false
+	}
+
+	return resp.IsSuccess()
+}
+*/
